3338b7a25b5e8e8bd506f2aa3641bdcaa6eb75ae
Catherine Ellaine Bagay <catherinebagay08@gmail.com>
2025-06-05 15:58:29 +0800

    feat: :art:

--- backend/migrate.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('database.db')
-conn.execute('ALTER TABLE emergency ADD COLUMN is_acknowledged INTEGER DEFAULT 0')
-conn.commit()
-conn.close()
-
-print("Column added.")
